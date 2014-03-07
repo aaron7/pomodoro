f57468a0ee57d58102f231cc4594095ed42ebca6
Aaron Kirkbride <ak759@cam.ac.uk>
2014-03-07 23:38:00 +0000

    Add hello world server

--- web/web.py
+++ web/web.py
@@ -1,10 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def show_stats():
+    return "Hello, world!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
